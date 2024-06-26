8ddf7a9738280ebad01470ff3ddbc092eb69b748
kilyanF <kilyan.fta@gmail.com>
2024-06-27 00:58:32 +0200

    Classe Pokemon

---
+++ test/Pokemon.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+using namespace std;
+
+
+class Pokemon {
+private:
+    int hp;
+    int xp;
+    int level;
+    string name;
+    //Faire une class Type
+    string type;
+    bool estKo;
+    //Faire une class Attaque
+    string attaques;
+
+public:
+    // Accesseurs
+    int GetHp();
+    int GetXp();
+    int GetLvl();
+    string GetName();
+    string GetType();
+    bool GetKo();
+    string GetAttaques();
+    // Mutateurs
+    void SetHp(int newHp);
+    void SetXp(int newXp);
+    void SetLvl(int newLvl);
+    void SetName(string newName);
+    void SetType(string newType);
+    void SetKo(bool newKo);
+    void SetAttaques(string newAttaques);
+    // Constructeur
+    Pokemon(int hp, int xp, int lvl, string name, string type, string attaques);
+};
+
+// Definition du constructeur
+Pokemon::Pokemon(int hp, int xp, int lvl, string name, string type, string attaques)
+{
+    this->hp = hp;
+    this->xp = xp;
+    this->level = lvl;
+    this->name = name;
+    this->type = type;
+    this->attaques = attaques;
+    this->estKo = false;
+}
+
+// Definition des méthodes
+// Accesseurs
+
+int Pokemon::GetHp()
+{
+    return this->hp;
+}
+
+int Pokemon::GetXp()
+{
+    return this->xp;
+}
+
+int Pokemon::GetLvl()
+{
+    return this->level;
+}
+
+string Pokemon::GetName()
+{
+    return this->name;
+}
+
+string Pokemon::GetType()
+{
+    return this->type;
+}
+
+bool Pokemon::GetKo()
+{
+    return this->estKo;
+}
+
+string Pokemon::GetAttaques()
+{
+    return this->attaques;
+}
+
+//Mutateurs
+
+void Pokemon::SetHp(int newHp)
+{   
+    this->hp = newHp;
+}
+
+void Pokemon::SetXp(int newXp)
+{   
+    this->xp = newXp;
+}
+
+void Pokemon::SetLvl(int newLvl)
+{   
+    this->level = newLvl;
+}
+
+void Pokemon::SetName(string newName)
+{   
+    this->name = newName;
+}
+
+void Pokemon::SetType(string newType)
+{   
+    this->type = newType;
+}
+
+void Pokemon::SetKo(bool newKo)
+{   
+    this->estKo = newKo;
+}
+
+void Pokemon::SetAttaques(string newAttaques)
+{   
+    this->attaques = newAttaques;
+}
